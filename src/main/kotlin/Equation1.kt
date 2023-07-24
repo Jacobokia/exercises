4077f3ca43ae9fd85bd538931b8d2cee6e3bdef1
Jacobokia <cobs399@gmail.com>
2023-07-24 15:52:50 +0300

    make changes to the bulldozer formula

--- src/main/kotlin/Equation1.kt
+++ src/main/kotlin/Equation1.kt
@@ -7,8 +7,8 @@ fun main(){
     val b = 10
     val c = 25
 
-    val y1 = b + sqrt((b*b -(4 * a * c)).toDouble()) /2*a
+    val y1 = (-b + (sqrt((b*b -(4 * a * c)).toDouble()))) /2*a
     println("Value of y1 is $y1")
-    val y2 = b - sqrt((b*b -(4 * a * c)).toDouble()) /2*a
+    val y2 = (-b - (sqrt((b*b -(4 * a * c)).toDouble()))) /2*a
     print("Value of y2 is $y2")
 }
--- src/main/kotlin/Equation4.kt
+++ src/main/kotlin/Equation4.kt
@@ -6,8 +6,8 @@ fun main(){
     val b = 7
     val c = 10
 
-    val x1 = b + sqrt((b*b -(4 * a * c)).toDouble())/2*a
+    val x1 = (-b + (sqrt((b*b -(4 * a * c)).toDouble())))/2*a
     println("Value of X1 is $x1")
-    val x2 = b - sqrt((b*b -(4 * a * c)).toDouble())/2*a
+    val x2 =(-b - (sqrt((b*b -(4 * a * c)).toDouble()))) /2*a
     print("Value of X2 is $x2")
 }
